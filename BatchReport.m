fb98972de202781963f0bdf792f737a495e5eecc
Geoffrey Jones <jonesg5@rpi.edu>
2013-03-22 16:02:41 -0400

    Enabled batching of reports

--- BatchReport.m
+++ BatchReport.m
@@ -3,22 +3,22 @@ clc
 
 load ProcessedData.mat ProcessedData FileDir
 
-% % Make directory for reports if it does not exist
-% PlotDir = fullfile(FileDir,'DaysimeterReports');
-% if isdir(PlotDir) == 0
-%     mkdir(PlotDir);
-% else
-%     rmdir(PlotDir,'s');
-%     mkdir(PlotDir);
-% end
+% Make directory for reports if it does not exist
+PlotDir = fullfile(FileDir,'DaysimeterReports');
+if isdir(PlotDir) == 0
+    mkdir(PlotDir);
+else
+    rmdir(PlotDir,'s');
+    mkdir(PlotDir);
+end
 % Plot files
 close all
 
 n = length(ProcessedData);
-% for i1 = 1:n
-i1 = 3;
+for i1 = 1:n
     DaysimeterReport(ProcessedData{i1}.subject,ProcessedData{i1}.time,...
         ProcessedData{i1}.lux,ProcessedData{i1}.CS,...
         ProcessedData{i1}.activity);
-    print(gcf,'-dpdf','test.pdf');
-% end
+    pdfName = fullfile(PlotDir,[ProcessedData{i1}.subject,'.pdf']);
+    print(gcf,'-dpdf',pdfName);
+end
